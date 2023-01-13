f8aaf2944f9077d92711ef2f57e9817b46ed8b5d
Lakshyasinghrawat12 <122603867+Lakshyasinghrawat12@users.noreply.github.com>
2023-01-13 16:09:16 +0530

    Add files via upload

---
+++ datatypes_in_python.py
@@ -0,0 +1,8 @@
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
