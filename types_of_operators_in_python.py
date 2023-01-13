4dd594b3cc35860fcd89b969259a29c397570a08
Lakshyasinghrawat12 <122603867+Lakshyasinghrawat12@users.noreply.github.com>
2023-01-13 16:02:54 +0530

    Types of operators in python

---
+++ types_of_operators_in_python.py
@@ -0,0 +1,26 @@
+ # addition(+)
+print(2 + 3)
+
+
+# subtraction(-)
+print(3 - 1)      
+
+
+# multiplication(*)
+print(2 * 3)
+
+
+# division(/)
+print(3 / 2)
+
+
+# exponential(**)- We use this operator between the base and exponent values.
+print(3 ** 2)    
+
+
+ # modulus(%)- It gives the remainder when the first number is divided from the second number.
+print(3 % 2)
+
+
+# Floor division operator(//)- This operator will divide the first argument by the second and round the result down to the nearest whole numbe
+print(3 // 2)            
